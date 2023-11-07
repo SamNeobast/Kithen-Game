f4ce957ec33248e4254afa1a8a3d753b54e0459e
Sviatoslav <116236590+SamNeobast@users.noreply.github.com>
2023-11-07 07:11:59 +0200

    Revert "Change kithen object parent"
    This reverts commit 4a40edd7a8aead51210220a81e874063ef8469d2.

--- Assets/Script/ClearCounter.cs
+++ Assets/Script/ClearCounter.cs
@@ -4,59 +4,11 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private Transform topKithenPoint;
     [SerializeField] private KithenObjectSO kithenObjectSO;
-    [SerializeField] private ClearCounter secondClearCounter;
-
-    [SerializeField] private bool testing;
-
-    private KithenObject kithenObject;
-
-    private void Update()
-    {
-        if (testing && Input.GetKeyDown(KeyCode.T))
-        {
-            if (kithenObject != null)
-            {
-                kithenObject.SetClearCounter(secondClearCounter);
-            }
-        }
-
-    }
-
     public void Interact()
     {
-        if (kithenObject == null)
-        {
-            Transform kithenObjectTransform = Instantiate(kithenObjectSO.prefab, topKithenPoint);
-            kithenObjectTransform.GetComponent<KithenObject>().SetClearCounter(this);
-        }
-        else
-        {
-            Debug.Log(kithenObject.GetClearCounter());
-        }
-    }
+        Transform kithenObjectSOTransform = Instantiate(kithenObjectSO.prefab, topKithenPoint);
+        kithenObjectSOTransform.localPosition = Vector3.zero;
 
-    public Transform GetTopKithenPointFollowTransform()
-    {
-        return topKithenPoint;
-    }
-
-    public void SetKithenObject(KithenObject kithenObject)
-    {
-        this.kithenObject = kithenObject;
-    }
-
-    public KithenObject GetKithenObject()
-    {
-        return kithenObject;
-    }
-
-    public void ClearKithenObject()
-    {
-        kithenObject = null;
-    }
-
-    public bool HasKithenObject()
-    {
-        return kithenObject != null;
+        Debug.Log(kithenObjectSOTransform.GetComponent<KithenObject>().GetKithenObjectSO().objectName);
     }
 }
--- Assets/Script/KithenObject.cs
+++ Assets/Script/KithenObject.cs
@@ -4,32 +4,5 @@ public class KithenObject : MonoBehaviour
 {
     [SerializeField] private KithenObjectSO kithenObjectSO;
 
-    private ClearCounter clearCounter;
-
-    public void SetClearCounter(ClearCounter newClearCounter)
-    {
-        if (clearCounter != null)
-        {
-            clearCounter.ClearKithenObject();
-        }
-
-        clearCounter = newClearCounter;
-
-        if (clearCounter.HasKithenObject())
-        {
-            Debug.LogError("Counter already has object");
-        }
-      
-        clearCounter.SetKithenObject(this);
-
-        transform.parent = clearCounter.GetTopKithenPointFollowTransform();
-        transform.localPosition = Vector3.zero;
-    }
-
-    public ClearCounter GetClearCounter()
-    {
-        return clearCounter;
-    }
-
     public KithenObjectSO GetKithenObjectSO() { return kithenObjectSO; }
 }
