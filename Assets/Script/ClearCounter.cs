d6da4823be16ea37913819bd6ca0182602cc46a9
Sviatoslav <116236590+SamNeobast@users.noreply.github.com>
2023-11-09 08:09:27 +0200

    Revert "Revert "Change kithen object parent""
    This reverts commit f4ce957ec33248e4254afa1a8a3d753b54e0459e.

--- Assets/Script/ClearCounter.cs
+++ Assets/Script/ClearCounter.cs
@@ -4,11 +4,59 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private Transform topKithenPoint;
     [SerializeField] private KithenObjectSO kithenObjectSO;
+    [SerializeField] private ClearCounter secondClearCounter;
+
+    [SerializeField] private bool testing;
+
+    private KithenObject kithenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kithenObject != null)
+            {
+                kithenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+
+    }
+
     public void Interact()
     {
-        Transform kithenObjectSOTransform = Instantiate(kithenObjectSO.prefab, topKithenPoint);
-        kithenObjectSOTransform.localPosition = Vector3.zero;
+        if (kithenObject == null)
+        {
+            Transform kithenObjectTransform = Instantiate(kithenObjectSO.prefab, topKithenPoint);
+            kithenObjectTransform.GetComponent<KithenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kithenObject.GetClearCounter());
+        }
+    }
 
-        Debug.Log(kithenObjectSOTransform.GetComponent<KithenObject>().GetKithenObjectSO().objectName);
+    public Transform GetTopKithenPointFollowTransform()
+    {
+        return topKithenPoint;
+    }
+
+    public void SetKithenObject(KithenObject kithenObject)
+    {
+        this.kithenObject = kithenObject;
+    }
+
+    public KithenObject GetKithenObject()
+    {
+        return kithenObject;
+    }
+
+    public void ClearKithenObject()
+    {
+        kithenObject = null;
+    }
+
+    public bool HasKithenObject()
+    {
+        return kithenObject != null;
     }
 }
